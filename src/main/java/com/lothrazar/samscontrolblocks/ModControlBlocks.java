d6842c79c7c28946ad412d90652a356e3025eb47
sam bassett <samson.bassett@gmail.com>
2015-06-09 10:44:08 -0700

    proxy bugfixes

--- src/main/java/com/lothrazar/samscontrolblocks/ModControlBlocks.java
+++ src/main/java/com/lothrazar/samscontrolblocks/ModControlBlocks.java
@@ -37,13 +37,13 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod(modid = ModControlBlocks.MODID, version = ModControlBlocks.VERSION,	name = ModControlBlocks.NAME, useMetadata = true )  
 public class ModControlBlocks
 {
-	public static final String MODID = "samskeys"; 
+	public static final String MODID = "samscontrolblocks"; 
 	public static final String TEXTURE_LOCATION = MODID + ":";
 	public static final String VERSION = "1.8-1.0.0";
-	public static final String NAME = "Sam's Keys and Macros";
+	public static final String NAME = "Sam's Block Control";
 	@Instance(value = MODID)
 	public static ModControlBlocks instance;
-	@SidedProxy(clientSide="com.lothrazar.samskeys.proxy.ClientProxy", serverSide="com.lothrazar.samskeys.proxy.CommonProxy")
+	@SidedProxy(clientSide="com.lothrazar.samscontrolblocks.proxy.ClientProxy", serverSide="com.lothrazar.samscontrolblocks.proxy.CommonProxy")
 	public static CommonProxy proxy;   
 	public static Logger logger; 
 	public static SimpleNetworkWrapper network;  
